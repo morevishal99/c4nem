50da0ef41c6c243250d8684f8fd8308a1a8d6bfb
morevishal99 <morevishal060298@gmail.com>
2023-03-27 17:17:01 +0530

    ok

--- middleware/Authentication.js
+++ middleware/Authentication.js
@@ -6,8 +6,6 @@ const Authentication=(req,res,next)=>{
         if(decoded){
             req.body.userid=decoded.userid;
             next()
-        }else{
-            res.status(400).send({"msg":"login fisrt"})
         }
     }else{
         res.status(400).send({"msg":"login fisrt"})
